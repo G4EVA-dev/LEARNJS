4166b495dbe56beba026dbf2aace5333bc543646
G4EVA-dev <tanzeglenn@gmail.com>
2023-01-03 21:33:32 +0100

    learn functions

--- script.js
+++ script.js
@@ -1,27 +1,5 @@
-var myVar = 'Global Variable'
 
-function checkScope( ){
-    myVar = 'Local Variable'
-    console.log(myVar )
+function sayHello(){
+    document.write("Hello World!");
 }
 
-var a = 10;
-var b = 20;
-var linebreak = "<br />";
-
-
-document.write ("((a > b) ? 100 : 200 => ");
-result = (a > b) ? 100 : 200;
-document.write(result);
-document.write(linebreak);
-
-if (b > a) {
-    document.write("<b>TRUE</b>");
-}
-
-var aProperty;
-for (aProperty in navigator)
-{
-    document.write(aProperty)
-    document.write("<br/>")
-}
