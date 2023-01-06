704983c10926beb6011d96794d4cc6379ff1802b
G4EVA-dev <tanzeglenn@gmail.com>
2023-01-06 04:28:10 +0100

    dunctions, dialog box

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 
 function sayHello(){
-    document.write("Hello World!");
+    alert("Hello World!");
 }
 
+function over() {
+    document.write ("Mouse Over");
+    }
+
+    function out() {
+        document.write ("Mouse Out");
+        }
+
+   // How to use the Confirmation Dialog Box
+function getConfirmation(){
+    var retVal = confirm("Do you want to continue ?");
+    if(retVal == true){
+        document.write("User wants to continue!");
+        console.log("TRUE");
+        return true; 
+    }
+    else{
+        document.write("User does not want to continue!");
+        console.log("FALSE");
+        return false;
+    }
+}  
+
+function getValue(){
+    var getVal = prompt("Enter your name: ", " your name here");
+    document.write("You have entered: " + getVal);
+}
